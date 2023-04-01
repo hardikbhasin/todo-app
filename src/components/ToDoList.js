cab81a1e60845f04bad6f28d7a871697242361af
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-01 22:33:06 +0530

    edited ToDoList component

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -2,8 +2,20 @@ import React , {useState} from 'react'
 import ToDoForm from './ToDoForm'
 
 export default function ToDoList() {
+    const [todos, setTodos] = useState([]);
+    const addToDo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+        const newTodos = [todo, ...todos];
+        setTodos(newTodos);
+       
+    }
+
   return (
     <div>
+        <h1>What's the plan for today?</h1>
+        <ToDoForm onSubmit={addToDo}/>
       
     </div>
   )
