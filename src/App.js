31afcdad1bc4457bce6107230368bc09fb5a7b25
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-01 22:31:58 +0530

    added todolist to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import ToDoForm from './components/ToDoForm';
+
+import ToDoList from './components/ToDoList';
 
 function App() {
   return (
     <div className="todo-app">
-      <ToDoForm />
+      <ToDoList />
+
  
     </div>
   );
