6e683c53094a07e56a6f9883a19e27c1cc3a203b
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-02 18:52:13 +0530

    update ToDo.js

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ToDoForm from './ToDoForm'
 
 function ToDo() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [edit, setEdit] = useState({
+        id: null,
+        value: ''
+    });
+    return ToDoList.map((ToDo, index) => (
+        <div className={ToDo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+            <div key={ToDo.id} onClick={() => completeToDo(ToDo.id)}>
+                {ToDo.text}
+            </div>
+            <div className='icons'>
+                
+            </div>
+        </div>
+    ))
 }
 
 export default ToDo
