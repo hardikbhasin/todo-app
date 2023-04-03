d1ed890a6abee242932062ddcfa4450e0320e725
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-03 21:54:18 +0530

    creating custom edit input

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,34 +1,44 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 function ToDoForm(props) {
-    const [input,setInput] = useState('');
+    const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
     const inputRef = useRef(null);
-    useEffect(() =>{
+    useEffect(() => {
         inputRef.current.focus()
 
     })
-    
-    const handleChange = e =>{
+
+    const handleChange = e => {
         setInput(e.target.value);
     };
-    const handleSubmit = e =>{
+    const handleSubmit = e => {
         e.preventDefault();
         props.onSubmit({
             id: Math.floor(Math.random() * 10000),
             text: input
         });
-        
+
         setInput('');
-        
+
     };
-  return (
-   <form className="todo-form" onSubmit={handleSubmit}>
-    <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange}
-    ref={inputRef}/>
-    <button className='todo-button'>Add todo</button>
-   </form>
-  )
+    return (
+        <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+                <>
+                    <input type="text" placeholder="Update your item" value={input} name="text" className="todo-input edit" onChange={handleChange}
+                        ref={inputRef} />
+                    <button className='todo-button edit'>Update</button>
+                </>) :
+                (
+                    <>
+                        <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange}
+                            ref={inputRef} />
+                        <button className='todo-button'>Add todo</button>
+                    </>)}
+
+        </form>
+    )
 }
 
 export default ToDoForm
