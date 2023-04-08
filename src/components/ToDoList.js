2ddbf67bf14061cbba74b54130f48bd4a88bef58
hardikbhasin <hardikbhasin398@gmail.com>
2023-04-08 21:05:04 +0530

    updated todolist.js

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -43,6 +43,7 @@ export default function ToDoList() {
          todos={todos} 
          completeToDo={completeToDo}
          removetodo={removetodo}
+
          updatedToDo={updatedToDo}/>
       
     </div>
